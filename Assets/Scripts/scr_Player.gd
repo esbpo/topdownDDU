205fe2ed0ce7f2bd455c8cb3e2f582467ba31c87
Rasmus Kolling <rasmuskolling26@gmail.com>
2026-08-21 12:48:09 +0200

    Added player movement

---
+++ Assets/Scripts/scr_Player.gd
@@ -0,0 +1,12 @@
+extends CharacterBody2D
+
+@export var speed: float = 200
+
+func GetInput():
+	var inputDirection = Input.get_vector("left", "right", "up", "down")
+	velocity = inputDirection * speed
+	
+func _physics_process(delta):
+	GetInput()
+	move_and_slide()
+	
