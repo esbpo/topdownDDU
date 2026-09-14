2d3f154f52d46cea9936d29707f92d13e52309a5
rall2607 <rasmuskolling26@gmail.com>
2026-09-14 08:24:12 +0200

    Hvert 5 level får man lov til at unlock et nyt våben

--- Assets/Scripts/scr_ShowUpgrades.gd
+++ Assets/Scripts/scr_ShowUpgrades.gd
@@ -2,14 +2,20 @@ extends CanvasLayer
 var instance
 @onready var scene = preload("res://scn_ChooseUpgrade.tscn")
 @onready var UpgradeJson: String = FileAccess.get_file_as_string("res://Data/Upgrades.json")
+@onready var WeaponUnlockJson : String = FileAccess.get_file_as_string("res://Data/WeaponUnlocks.json")
+
+var weaponUnlockDict: Dictionary
 var upgradeDict: Dictionary
 var upgradesAvailable 
+var weaponsAvailable
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	upgradeDict = JSON.parse_string(UpgradeJson)
 	upgradesAvailable = len(upgradeDict)
-
+	weaponUnlockDict = JSON.parse_string(WeaponUnlockJson)
+	weaponsAvailable = len(weaponUnlockDict)
+	
 
 func ShowUpgrades(): 
 	instance = scene.instantiate()
@@ -18,6 +24,7 @@ func ShowUpgrades():
 	visible = true
 	#Pauses the game so player has time to think when choosing
 	get_tree().paused = true #Also works to stop upgrade choices overlapping if player somehow levels up twice
+	
 	
 func HideUpgrades():
 	instance.queue_free()
